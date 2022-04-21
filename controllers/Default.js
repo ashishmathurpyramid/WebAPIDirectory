0aa279c821b145e04ba98c290b3c1bddda5cb9f6
Ashish Mathur <ashish.mathur@pyramidconsultinginc.com>
2022-04-21 13:20:04 +0530

    reverted MSAL changes

--- controllers/Default.js
+++ controllers/Default.js
@@ -2,16 +2,13 @@
 
 var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
-const validateAuthToken = require('./validateAuthToken');
 
 module.exports.getWebAPIGET = function getWebAPIGET(req, res, next, webAPIName) {
-  validateAuthToken.validateJwt(req, res, (res) => {
-    Default.getWebAPIGET(webAPIName)
-      .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response.Message, response.Status);
-      });
-  });
+  Default.getWebAPIGET(webAPIName)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response.Message, response.Status);
+    });
 };
